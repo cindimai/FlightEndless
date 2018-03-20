f6aba12dc671e28c40b4d896706eb03cf89f3f4e
cindimai <cindi.1110@yahoo.com>
2018-03-20 16:13:26 -0400

    ALL Files

---
+++ Assets/FlightChallenge/Scripts/EnemyDestroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroy : MonoBehaviour
+{
+
+	// Use this for initialization
+	public int health;
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		health--;
+		if (health <= 0) {
+			Destroy (gameObject);
+			Destroy (collision.gameObject);
+			ScoreManager.score = ScoreManager.score + 1;
+		}
+		Debug.Log ("trigger");
+	}
+}
